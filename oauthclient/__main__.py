f4ad2b7ac6133d6f914e19a31023db06a1e8db97
Kyle Robbertze <kyle@paddatrapper.com>
2020-02-29 11:35:43 +0200

    finally get it working

--- oauthclient/__main__.py
+++ oauthclient/__main__.py
@@ -6,26 +6,20 @@ import os
 API_HOST = 'http://localhost:8090'
 OAUTH_TOKEN_ENDPOINT = '/oauth/token/'
 API_ENDPOINT = '/api/v2_1/properties/'
-# Development instance ID. Change to match your environment
-CLIENT_ID = '2R6TunDgVi6wmKzRtcGzOYNTRKmyYr8eNXPMu17Q'
-# Development instance secret. Change to match your environment
-CLIENT_SECRET = 'aiCzrBL9BX7JQXDluKmX6TAUpU6uiHBITCXGmywfHVWjJ9bGHORXIE611SKuK7bMhOQbfJhKnzg61fMQKCiICVTAE29aWjtSKmEgJp2VJCamAdKfIpmpdTwbn4UfOZ6k'
 
 def main():
-    #client_id = input('Enter the client ID: ')
-    #client_secret = input('Enter the client secret: ')
-    if False:
-        r = requests.get(API_HOST)
-        csrf_token = r.cookies['csrftoken']
+    client_id = input('Enter the client ID: ')
+    client_secret = input('Enter the client secret: ')
+    username = input('Enter your username: ')
+    password = input('Enter your password: ')
 
     params = {
-        'client_id': CLIENT_ID,
-        'client_secret': CLIENT_SECRET,
-        #'csrfmiddlewaretoken': csrf_token,
-        'grant_type': 'client_credentials',
+        'username': username,
+        'password': password,
+        'grant_type': 'password',
     }
     token_response = requests.post(API_HOST + OAUTH_TOKEN_ENDPOINT,
-                                   params=params)
+                                   params=params, auth=(client_id, client_secret))
     token_json = token_response.json()
     access_token = token_json['access_token']
     scope = token_json['scope']
@@ -33,8 +27,7 @@ def main():
     print(f'Scope: {scope}')
 
     params = {
-        #'csrfmiddlewaretoken': csrf_token,
-        'org_id': '2',
+        'org_id': '1',
     }
     headers = {
         'content-type':'application/json',
